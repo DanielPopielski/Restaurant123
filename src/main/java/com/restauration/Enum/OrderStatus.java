e7c0dadd3743b93eb6e3b5f0855d311ca2c505b4
DanielPopielski <danielek6699@wp.pl>
2023-09-18 22:24:31 +0200

    deleted

--- src/main/java/com/restauration/Enum/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.restauration.Enum;
-
-public enum OrderStatus {
-    NEW,
-    IN_PROGRESS,
-    READY,
-    DELIVERED,
-    CANCELLED
-}
--- src/main/java/com/restauration/Security/JwtUtils.java
+++
@@ -1,2 +0,0 @@
-package com.restauration.Security;public class JwtUtils {
-}
